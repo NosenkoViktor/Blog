4987ca03e82deff53e18b84ff6a40e6488b072f5
NosenkoViktor <vtk_@mail.ru>
2016-09-15 20:20:44 +0300

    v1.0

--- BlogProject/Controllers/AccountController.cs
+++ BlogProject/Controllers/AccountController.cs
@@ -1,22 +1,55 @@
-﻿using System;
+﻿using BlogProject.Concrete;
+using BlogProject.Models;
+using BlogProject.Models.AccountModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace BlogProject.Controllers
 {
     public class AccountController : Controller
     {
+        public ActionResult Register()
+        {
+            return View();
+        }
 
         public ActionResult LogIn()
         {
             return View();
         }
 
-        public ActionResult Register()
+        [HttpPost]
+        public ActionResult Register(RegisterUserModel model)
         {
-            return View();
+            UserModel user = new UserModel();
+            EFDbContext context = new EFDbContext();
+            int count = context.Users.Count(u => u.Username == model.Username);
+            if (ModelState.IsValid && count == 0)
+            {
+                user.Username = model.Username;
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.Email = model.Email;
+                user.Password = model.Password;
+                context.Users.Add(user);
+                context.SaveChanges();
+                return RedirectToAction("logIn", "Account");
+            }
+            else
+            {
+                ModelState.AddModelError("", "This Username is already in use. Please choose another Username.");
+                return View();
+            }
+        }
+
+        public JsonResult CheckUserName(string username)
+        {
+            var result = Membership.FindUsersByName(username).Count == 0;
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- BlogProject/Models/AccountModels/RegisterUserModel.cs
+++ BlogProject/Models/AccountModels/RegisterUserModel.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using BlogProject.Concrete;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BlogProject.Models.AccountModels
 {
     public class RegisterUserModel
     {
         [Required(ErrorMessage="Enter User name please")]
+        [Remote("CheckUserName", "Account")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Enter First name please")]
@@ -20,10 +23,16 @@ namespace BlogProject.Models.AccountModels
         [Required(ErrorMessage = "Enter E-mail name please")]
         public string Email { get; set; }
 
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The value of {0} must contain not less than {2} characters .", MinimumLength = 6)]
+        [Display(Name = "Password")]
         [Required(ErrorMessage = "Enter Password name please")]
         public string Password { get; set; }
 
+        [DataType(DataType.Password)]
         [Required(ErrorMessage = "Repeat password name please")]
+        [System.Web.Mvc.Compare("Password", ErrorMessage = "Password and confirmation do not match.")]
         public string RepeatPassword { get; set; }
+
     }
 }
